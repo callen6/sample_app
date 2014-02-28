ec56dfec90276400617444b876411743f6a2ae2b
Cori Allen <coriallen@gmail.com>
2014-02-28 11:57:53 -0500

    Add edit and update actions to users controller, tests to authenticate request specs and user pages specs.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -21,14 +21,17 @@ describe "AuthenticationPages" do
 
 		describe "with valid information" do
 			let(:user) { FactoryGirl.create(:user) }
-			before do
-				fill_in "Email", with: user.email.upcase
-				fill_in "Password", with: user.password
-				click_button "Sign in"
-			end
+			# This gets moved to the helper in utilities.rb
+			# before do
+			# 	fill_in "Email", with: user.email.upcase
+			# 	fill_in "Password", with: user.password
+			# 	click_button "Sign in"
+			# end
+			before {sign_in user}
 
 			it { should have_title(user.name) }
 			it { should have_link('Profile', href: user_path(user)) }
+			it { should have_link("Settings", href: edit_user_path(user))}
 			it { should have_link('Sign out', href: signout_path) }
 			it { should_not have_link("Sign in", href: signin_path) }
 
@@ -37,5 +40,24 @@ describe "AuthenticationPages" do
 				it {should have_link "Sign in"}
 			end
 		end
+  end
+
+  describe "authorization" do
+  	describe "for non signed-in users" do
+  		let(:user) { FactoryGirl.create(:user)}
+
+  		describe "in the Users controller" do
+
+  			describe "visiting the edit page" do
+  				before {visit edit_user_path(user)}
+  				it { should have_title("Sign in")}
+  			end
+
+  			describe "submiting the update action" do
+  				before { patch user_path(user)}
+  				specify {expect(response).to redirect_to(signin_path)}
+  			end
+  		end
+  	end
   end
 end
